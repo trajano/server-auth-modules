cb9412d6c1a36b5e16d26ad063e87d85fba3f90f
Archimedes Trajano <arch@trajano.net>
2016-10-30 16:23:36 -0400

    Updated to make tests work
    Heroku test has been removed.  Google URL has been updated.  Selenium no
    longer used.

--- src/test/java/net/trajano/auth/test/AuthSequenceIT.java
+++ src/test/java/net/trajano/auth/test/AuthSequenceIT.java
@@ -1,7 +1,7 @@
 package net.trajano.auth.test;
 
 import static net.trajano.auth.OAuthModule.REDIRECTION_ENDPOINT_URI_KEY;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -14,82 +14,86 @@ import javax.security.auth.message.MessageInfo;
 import javax.security.auth.message.MessagePolicy;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.AssertTrue;
 
 import net.trajano.auth.OAuthModule;
 import net.trajano.auth.OpenIDConnectAuthModule;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.google.common.collect.ImmutableMap;
 
 public class AuthSequenceIT {
 
-    @Test
-    public void testRedirectToEndpoint() throws Exception {
+	@Test
+	public void testRedirectToEndpoint() throws Exception {
 
-        final OpenIDConnectAuthModule module = new OpenIDConnectAuthModule();
+		final OpenIDConnectAuthModule module = new OpenIDConnectAuthModule();
 
-        final MessagePolicy mockRequestPolicy = mock(MessagePolicy.class);
-        when(mockRequestPolicy.isMandatory()).thenReturn(true);
-        final Map<String, String> options = ImmutableMap.<String, String> builder()
-                .put(OpenIDConnectAuthModule.ISSUER_URI_KEY, "https://accounts.google.com")
-                .put(REDIRECTION_ENDPOINT_URI_KEY, "/app/oauth2")
-                .put(OAuthModule.CLIENT_ID_KEY, "clientID")
-                .put(OAuthModule.CLIENT_SECRET_KEY, "clientSecret")
-                .build();
-        module.initialize(mockRequestPolicy, null, null, options);
+		final MessagePolicy mockRequestPolicy = mock(MessagePolicy.class);
+		when(mockRequestPolicy.isMandatory()).thenReturn(true);
+		final Map<String, String> options = ImmutableMap.<String, String>builder()
+				.put(OpenIDConnectAuthModule.ISSUER_URI_KEY, "https://accounts.google.com")
+				.put(REDIRECTION_ENDPOINT_URI_KEY, "/app/oauth2").put(OAuthModule.CLIENT_ID_KEY, "clientID")
+				.put(OAuthModule.CLIENT_SECRET_KEY, "clientSecret").build();
+		module.initialize(mockRequestPolicy, null, null, options);
 
-        final MessageInfo messageInfo = mock(MessageInfo.class);
+		final MessageInfo messageInfo = mock(MessageInfo.class);
 
-        final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
-        when(servletRequest.getMethod()).thenReturn("GET");
-        when(servletRequest.getRequestURL()).thenReturn(new StringBuffer("https://i.trajano.net:8443/util/ejb2"));
-        when(servletRequest.getRequestURI()).thenReturn("/util/ejb2");
-        when(servletRequest.getContextPath()).thenReturn("/util");
-        when(servletRequest.isSecure()).thenReturn(true);
+		final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+		when(servletRequest.getMethod()).thenReturn("GET");
+		when(servletRequest.getRequestURL()).thenReturn(new StringBuffer("https://i.trajano.net:8443/util/ejb2"));
+		when(servletRequest.getRequestURI()).thenReturn("/util/ejb2");
+		when(servletRequest.getContextPath()).thenReturn("/util");
+		when(servletRequest.isSecure()).thenReturn(true);
 
-        when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
+		when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
 
-        final HttpServletResponse servletResponse = mock(HttpServletResponse.class);
-        when(messageInfo.getResponseMessage()).thenReturn(servletResponse);
+		final HttpServletResponse servletResponse = mock(HttpServletResponse.class);
+		when(messageInfo.getResponseMessage()).thenReturn(servletResponse);
 
-        final Subject client = new Subject();
-        assertEquals(AuthStatus.SEND_CONTINUE, module.validateRequest(messageInfo, client, null));
-        verify(servletResponse).sendRedirect("https://accounts.google.com/o/oauth2/auth?client_id=clientID&response_type=code&scope=openid&redirect_uri=https://i.trajano.net:8443/app/oauth2&state=L2VqYjI");
-    }
+		final Subject client = new Subject();
+		assertEquals(AuthStatus.SEND_CONTINUE, module.validateRequest(messageInfo, client, null));
+		ArgumentCaptor<String> redirectCaptor = ArgumentCaptor.forClass(String.class);
+		verify(servletResponse).sendRedirect(redirectCaptor.capture());
+		assertTrue(redirectCaptor.getValue(), redirectCaptor.getValue().startsWith(
+				"https://accounts.google.com/o/oauth2/v2/auth?client_id=clientID&response_type=code&scope=openid&redirect_uri=https://i.trajano.net:8443/app/oauth2&state=L2VqYjI"));
+	}
 
-    @Test
-    public void testRedirectToEndpointWithQueryString() throws Exception {
+	@Test
+	public void testRedirectToEndpointWithQueryString() throws Exception {
 
-        final OpenIDConnectAuthModule module = new OpenIDConnectAuthModule();
+		final OpenIDConnectAuthModule module = new OpenIDConnectAuthModule();
 
-        final MessagePolicy mockRequestPolicy = mock(MessagePolicy.class);
-        when(mockRequestPolicy.isMandatory()).thenReturn(true);
-        final Map<String, String> options = ImmutableMap.<String, String> builder()
-                .put(OpenIDConnectAuthModule.ISSUER_URI_KEY, "https://accounts.google.com")
-                .put(REDIRECTION_ENDPOINT_URI_KEY, "/app/oauth2")
-                .put(OAuthModule.CLIENT_ID_KEY, "clientID")
-                .put(OAuthModule.CLIENT_SECRET_KEY, "clientSecret")
-                .build();
-        module.initialize(mockRequestPolicy, null, null, options);
+		final MessagePolicy mockRequestPolicy = mock(MessagePolicy.class);
+		when(mockRequestPolicy.isMandatory()).thenReturn(true);
+		final Map<String, String> options = ImmutableMap.<String, String>builder()
+				.put(OpenIDConnectAuthModule.ISSUER_URI_KEY, "https://accounts.google.com")
+				.put(REDIRECTION_ENDPOINT_URI_KEY, "/app/oauth2").put(OAuthModule.CLIENT_ID_KEY, "clientID")
+				.put(OAuthModule.CLIENT_SECRET_KEY, "clientSecret").build();
+		module.initialize(mockRequestPolicy, null, null, options);
 
-        final MessageInfo messageInfo = mock(MessageInfo.class);
+		final MessageInfo messageInfo = mock(MessageInfo.class);
 
-        final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
-        when(servletRequest.getMethod()).thenReturn("GET");
-        when(servletRequest.getRequestURL()).thenReturn(new StringBuffer("https://i.trajano.net:8443/util/ejb2"));
-        when(servletRequest.getRequestURI()).thenReturn("/util/ejb2");
-        when(servletRequest.getContextPath()).thenReturn("/util");
-        when(servletRequest.getQueryString()).thenReturn("q=foo");
-        when(servletRequest.isSecure()).thenReturn(true);
+		final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+		when(servletRequest.getMethod()).thenReturn("GET");
+		when(servletRequest.getRequestURL()).thenReturn(new StringBuffer("https://i.trajano.net:8443/util/ejb2"));
+		when(servletRequest.getRequestURI()).thenReturn("/util/ejb2");
+		when(servletRequest.getContextPath()).thenReturn("/util");
+		when(servletRequest.getQueryString()).thenReturn("q=foo");
+		when(servletRequest.isSecure()).thenReturn(true);
 
-        when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
+		when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
 
-        final HttpServletResponse servletResponse = mock(HttpServletResponse.class);
-        when(messageInfo.getResponseMessage()).thenReturn(servletResponse);
+		final HttpServletResponse servletResponse = mock(HttpServletResponse.class);
+		when(messageInfo.getResponseMessage()).thenReturn(servletResponse);
 
-        final Subject client = new Subject();
-        assertEquals(AuthStatus.SEND_CONTINUE, module.validateRequest(messageInfo, client, null));
-        verify(servletResponse).sendRedirect("https://accounts.google.com/o/oauth2/auth?client_id=clientID&response_type=code&scope=openid&redirect_uri=https://i.trajano.net:8443/app/oauth2&state=L2VqYjI_cT1mb28");
-    }
+		final Subject client = new Subject();
+		assertEquals(AuthStatus.SEND_CONTINUE, module.validateRequest(messageInfo, client, null));
+		ArgumentCaptor<String> redirectCaptor = ArgumentCaptor.forClass(String.class);
+		verify(servletResponse).sendRedirect(redirectCaptor.capture());
+		assertTrue(redirectCaptor.getValue().startsWith(
+				"https://accounts.google.com/o/oauth2/v2/auth?client_id=clientID&response_type=code&scope=openid&redirect_uri=https://i.trajano.net:8443/app/oauth2&state=L2VqYjI"));
+	}
 }
